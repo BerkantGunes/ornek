0b7a420c0a9f09392780323b80455e8f1d9e7ea2
Berkant Gunes <gunesberkant96@gmail.com>
2025-03-20 17:45:23 +0300

    Add files via upload

---
+++ createfilelesson.py
@@ -0,0 +1,4 @@
+file = open("newfile.txt","w")
+file.write("Bu dosya Python Dosya Yonetimi Dersinde deneme amacli olusturulmustur!")
+file.write("\nBerkant Gunes tarafindan olusturulmustur!")
+file.close()
